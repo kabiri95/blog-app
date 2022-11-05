2a0354943b49f93fde2bc651a2746232caafdbf8
kabiri95 <erfankabiri337@gmail.com>
2022-11-05 20:28:31 +0330

    test

--- blog/tests.py
+++ blog/tests.py
@@ -39,3 +39,24 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'a good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'new title',
+            'body':  'new text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'new title')
+        self.assertEqual(Post.objects.last().body, 'new text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'updated title',
+            'body': 'update text'
+        })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
